a6ce7b7edb0fa992aa0ad77ebc44e14241d3018d
ricardovg4 <ricardo.velasco.guachalla@gmail.com>
2021-05-14 17:56:08 -0400

    build export for out folder

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,14 +1,14 @@
 import headerNavLinks from '../data/headerNavLinks';
 import Link from 'next/link';
 
-import SocialIcon from '../components/social-icons';
-import siteMetadata from '../data/siteMetadata.json';
+import SectionContainer from '../components/SectionContainer';
+import Footer from '../components/Footer';
 
 function LayoutWrapper({ children }) {
     return (
         <div>
             <div className="mx-auto border-b border-gray-600">
-                <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
+                <SectionContainer>
                     <header className="flex items-center justify-between py-8">
                         <div>
                             <Link href="/" aria-label="rivendev blog">
@@ -25,40 +25,13 @@ function LayoutWrapper({ children }) {
                             ))}
                         </div>
                     </header>
-                </div>
+                </SectionContainer>
             </div>
 
-            <main className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
-                {children}
-            </main>
-
-            <footer>
-                <div className="flex flex-col items-center mt-16">
-                    <div className="flex mb-3 space-x-4">
-                        <SocialIcon
-                            kind="mail"
-                            href={`mailto:${siteMetadata.email}`}
-                            size="6"
-                        />
-                        <SocialIcon kind="github" href={siteMetadata.github} size="6" />
-                        <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
-                        <SocialIcon
-                            kind="linkedin"
-                            href={siteMetadata.linkedin}
-                            size="6"
-                        />
-                        <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
-                    </div>
-                    <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
-                        <div>{siteMetadata.author}</div>
-                        <div>{` • `}</div>
-                        <div>{`© ${new Date().getFullYear()}`}</div>
-                    </div>
-                    <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-                        <Link href="/">{siteMetadata.title}</Link>
-                    </div>
-                </div>
-            </footer>
+            <SectionContainer>
+                <main>{children}</main>
+                <Footer />
+            </SectionContainer>
         </div>
     );
 }
